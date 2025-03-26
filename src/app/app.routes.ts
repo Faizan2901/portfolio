42fc61ded4135b16fb1acb4dbd4f5c81ab82d718
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2025-03-26 15:38:13 +0530

    Initial portfolio designed is completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'skill',
+        component: SkillComponent
+    },
+    {
+        path: 'project',
+        component: ProjectComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
